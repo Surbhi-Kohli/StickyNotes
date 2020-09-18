088875438577144d66083c90a89dac0270e30656
surbhikohli <surbhikohli1995@gmail.com>
2020-09-18 14:56:23 +0530

    Updates

---
+++ precache-manifest.c859963c26e023159878c87d13077488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb936bf3034297eb7b02e4b34b5e77b",
+    "url": "/StickyNotes/index.html"
+  },
+  {
+    "revision": "c83f61e783355795f6a5",
+    "url": "/StickyNotes/static/css/main.8040fc56.chunk.css"
+  },
+  {
+    "revision": "36cc95196b98e4f07599",
+    "url": "/StickyNotes/static/js/2.71576044.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StickyNotes/static/js/2.71576044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83f61e783355795f6a5",
+    "url": "/StickyNotes/static/js/main.334e4553.chunk.js"
+  },
+  {
+    "revision": "6e3f6328000d899909b7",
+    "url": "/StickyNotes/static/js/runtime-main.7a22829a.js"
+  }
+]);
