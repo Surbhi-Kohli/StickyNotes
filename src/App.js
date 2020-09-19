9cf4c40e02ad5d50f8c7ea8e085030724cfb648d
surbhikohli <surbhikohli1995@gmail.com>
2020-09-19 16:45:04 +0530

    input type changed to text-area

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import del from "./assets/images/thrash.png";
 import "./App.css";
 
+
 class App extends Component {
   constructor(props) {
     super(props);
